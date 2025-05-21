ba91a27dfe39331eb8a1a5816bfa871a09d77a23
whitehairdevjs <jsuserwork20@gmail.com>
2025-05-21 21:09:53 +0900

    init spring

---
+++ src/main/java/com/example/js/JsApplication.java
@@ -0,0 +1,13 @@
+package com.example.js;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/js/JsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.js;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
