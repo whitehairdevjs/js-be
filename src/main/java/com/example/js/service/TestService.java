c4aa24191a65ad1883a9637b61f42e2130382ccf
whitehairdevjs <jsuserwork20@gmail.com>
2025-05-21 21:09:53 +0900

    init spring

---
+++ src/main/java/com/example/js/service/TestService.java
@@ -0,0 +1,10 @@
+package com.example.js.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+    public String getString() {
+        return "Hello World";
+    }
+}
