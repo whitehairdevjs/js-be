2b36b61adcd889e80f2667e0f0d4ee8c3569a8cf
whitehairdevjs <jsuserwork20@gmail.com>
2025-07-03 23:55:24 +0900

    service 하단의 모든 method 실행 시 실행 시간 찍히도록 aop 설정.

---
+++ src/main/java/com/example/js/aspect/PerformanceLoggingAspect.java
@@ -0,0 +1,26 @@
+package com.example.js.aspect;
+
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class PerformanceLoggingAspect {
+    private static final Logger log = LoggerFactory.getLogger(PerformanceLoggingAspect.class);
+    @Around("execution(* com.example.js.service..*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+
+        Object result = joinPoint.proceed();
+
+        long duration = System.currentTimeMillis() - start;
+        log.info("*^* {} excute time: {} ms", joinPoint.getSignature(), duration);
+
+        return result;
+    }
+}
